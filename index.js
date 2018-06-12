5d2332929e41bdc9bed5ab2b4cbdcc6615be3f0a
Kyle Stockmann <kyle_stockmann@yahoo.com>
2018-06-12 15:08:35 -0700

    Changed JS around But still not working

--- index.js
+++ index.js
@@ -1,34 +1,53 @@
 const apiKey = 'AIzaSyBtLihEEX2iv7wtWM8yKRrzsLuadJtL5NA'
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search'
+console.log(apiKey);
+console.log(YOUTUBE_SEARCH_URL);
 
-function getDataFromApi(searchTerm) {
+//ajax setup
+function getDataFromApi(searchTerm, callback) {
 	const settings = {
-		method: 'GET',
 		url: YOUTUBE_SEARCH_URL,
-		key: apiKey,
-		data: {
-			'maxResults': '5',
-			'part': 'snippet',
-			'q' : searchTerm,
-			'type': ''
-		}); 
+		data:{
+			part: 'snippet',
+			key: apiKey,
+			q: searchTerm,
+			maxResults: 5,
+			type: 'video',
+			dataType:'json',
+			type: 'GET',
+			success: callback,
+			}
+		};
 	$.ajax(settings);
 }
 
-
-function renderResult(item) {
-	console.log(item);
+//handle submit
+function handleSubmit () {
+	$('.js-search-form').on('submit', event => {
+		event.preventDefault();
+		console.log('Search');
+		const queryTarget = $('event.currentTarget').find('js-query');
+		const query = queryTarget.val();
+		getDataFromApi(query, displayYoutubeSearchData);
+	});
 }
 
-function handleSubmit() {
-	$('.js-search-form').submit(event => {
-		event.preventDefault;
-		const queryTarget = $(this).find('js-query');
-		const query = queryTarget.val();
-		console.log(queryTarget);
-		getDataFromApi(query);
 
-	})
+//access items in the obj for api map them to renderResult
+function displayYoutubeSearchData(data) {
+  const results = data.items.map((item, index) => renderResult(item));
+  $('.js-search-results').html(results);
+}
+
+//render values from displayYoutubeSearchData
+function renderResult(result) {
+  return `
+    <div>
+      <h2>
+      videos here!!
+      </h2>
+    </div>
+  `;
 }
 
 handleSubmit();
